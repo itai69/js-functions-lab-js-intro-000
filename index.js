0cc3008ca18958f283b2d1892815b01b62b3c8c8
itai rosenberg <djitai69@gmail.com>
2019-10-12 16:35:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ function happyHolidays() {
 }
 
 function happyHolidaysTo(name){
-  return (`Happy Holidays, ${name}!`)
+  return (`Happy holidays, ${name}!`)
 }
 
 function happyHolidayTo(holiday,name){
   return (`Happy ${holiday}, ${name}!`)
 }
 
-function holidayCountdown(days,holiday){
+function holidayCountdown(holiday,days){
   return (`It's ${days} days untill ${holiday}!`)
 }
